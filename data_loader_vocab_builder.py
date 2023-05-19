53ef8254fbcd97ff6def5bcf4788828b73f0ca1c
Muhi0709 <62468783+Muhi0709@users.noreply.github.com>
2023-05-19 09:57:05 +0530

    Updated data loader and train.py files

--- data_loader_vocab_builder.py
+++ data_loader_vocab_builder.py
@@ -3,8 +3,11 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 import numpy as np
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")   #setting device to cuda if it is available
+
+#build the dictionary for inputs(latin_script) and outputs(devanagari script:"Tamil")
+#character to index & index to character
 def build_character_corpus(x,y,latin_script_word2idx,latin_script_idx2word,dev_script_idx2word,dev_script_word2idx):
 
     N=len(x)
@@ -20,7 +23,7 @@ def build_character_corpus(x,y,latin_script_word2idx,latin_script_idx2word,dev_s
                 dev_script_word2idx[ele]=len(dev_script_word2idx)
                 dev_script_idx2word[len(dev_script_idx2word)]=ele
 
-
+#load data and convert letters to indices using the built corpus/script dictionary
 def words_to_indices(x,y,latin_script,dev_script_word2idx):
     N=len(x)
     max_length_x=-float("inf")
@@ -29,31 +32,31 @@ def words_to_indices(x,y,latin_script,dev_script_word2idx):
     y_list=[]
     seq_lens_x=[]
     seq_lens_y=[]
-    for i in range(N):
+    for i in range(N):         
         a=[]
-        for ele in x[i]:
+        for ele in x[i]:               #go over each character in the input(latin_script) word
             if ele in latin_script:
-                a.append(latin_script[ele])
+                a.append(latin_script[ele])    
             else:
-                a.append(latin_script["<un>"])
-        a.append(latin_script["<eow>"])
+                a.append(latin_script["<un>"])    #unallocated token for previously unseen character
+        a.append(latin_script["<eow>"])            # end of word token to mark the end of the input word
         seq_lens_x.append(len(a))
         max_length_x=max(max_length_x,len(a))
         x_list.append(a)
 
         a=[]
-        a.append(dev_script_word2idx["<sow>"])
-        for ele in y[i]:
+        a.append(dev_script_word2idx["<sow>"])            # append start of word token to mark the start of the prediction/output word
+        for ele in y[i]:                              #go over each character in the input(latin_script) word
             if ele in dev_script_word2idx:
                 a.append(dev_script_word2idx[ele])
             else:
-                a.append(dev_script_word2idx["<un>"])
-        a.append(dev_script_word2idx["<eow>"])
+                a.append(dev_script_word2idx["<un>"])   #unallocated token for previously unseen character
+        a.append(dev_script_word2idx["<eow>"])             # end of word token to mark the end of the prediction/output word
         seq_lens_y.append(len(a))
         max_length_y=max(max_length_y,len(a))
         y_list.append(a)
 
-    for i in range(N):
+    for i in range(N):                                      #find the maximum of sequence lengths to do padding with pad token(index:0)
         if len(x_list[i])<max_length_x:
             x_list[i]=x_list[i]+[0]*(max_length_x-len(x_list[i]))
         if len(y_list[i])< max_length_y:
@@ -61,21 +64,22 @@ def words_to_indices(x,y,latin_script,dev_script_word2idx):
     return torch.cat((torch.tensor(x_list),torch.tensor(y_list),torch.tensor(
         seq_lens_x).unsqueeze(1),torch.tensor(seq_lens_y).unsqueeze(1)),dim=1),max_length_x,max_length_y
 
-def load_data():
-    train_pairs=np.genfromtxt("C:/Users/muhilan/Downloads/aksharantar_sampled/aksharantar_sampled/tam/tam_train.csv",
+def load_data():   #load train,valid,test_pairs
+    train_pairs=np.genfromtxt("aksharantar_sampled/tam/tam_train.csv",
                           delimiter=",",encoding="utf-8",dtype=str)
     
-    val_pairs=np.genfromtxt("C:/Users/muhilan/Downloads/aksharantar_sampled/aksharantar_sampled/tam/tam_valid.csv",
+    val_pairs=np.genfromtxt("aksharantar_sampled/tam/tam_valid.csv",
                           delimiter=",",encoding="utf-8",dtype=str)
-    test_pairs=np.genfromtxt("C:/Users/muhilan/Downloads/aksharantar_sampled/aksharantar_sampled/tam/tam_test.csv",
+    test_pairs=np.genfromtxt("aksharantar_sampled/tam/tam_test.csv",
                           delimiter=",",encoding="utf-8",dtype=str)
     latin_script_word2idx={"<pad>":0,"<un>":1,"<eow>":2}
     latin_script_idx2word={0:"<pad>",1:"<un>",2:"<eow>"}
     dev_script_word2idx={"<pad>":0,"<sow>":1,"<eow>":2,"<un>":3}
     dev_script_idx2word={0:"pad",1:"<sow>",2:"<eow>",3:"<un>"}
 
-
+    #build corpus
     build_character_corpus(train_pairs[:,0],train_pairs[:,1],latin_script_word2idx,latin_script_idx2word,dev_script_idx2word,dev_script_word2idx)
+    #character to indices mapping becomes the new data
     train_data,max_length_x,max_length_y=words_to_indices(train_pairs[:,0],train_pairs[:,1],latin_script_word2idx,dev_script_word2idx)
     val_data,max_length_val_x,max_length_val_y=words_to_indices(val_pairs[:,0],val_pairs[:,1],latin_script_word2idx,dev_script_word2idx)
     test_data,max_length_test_x,max_length_test_y=words_to_indices(test_pairs[:,0],test_pairs[:,1],latin_script_word2idx,dev_script_word2idx)
@@ -84,3 +88,4 @@ def load_data():
     return (latin_script_word2idx,latin_script_idx2word,dev_script_word2idx,dev_script_idx2word),(
         train_data,max_length_x,max_length_y),(val_data,max_length_val_x,max_length_val_y),(test_data,
         max_length_test_x,max_length_test_y)
+#return corpus,train_data(character indices),test_data(character indices),valid_data(character indices)
--- train.py
+++ train.py
@@ -1,13 +1,16 @@
+#script for training and testing model from commandline argument
+
 import argparse
+#define argsparse arguments necessary for 'training' and 'compute_loss_accuracy'functions from main.py
 parser=argparse.ArgumentParser(prog="Training & Testing NN model",description="""Training and Testing the encoder-decoder network on the 
 given aksharantar dataset.All parse arguments have default values and are set to the best configuration obtained from wandb sweep experiments""")
 parser.add_argument('-wp','--wandb_project',default="cs6910_assignment3",
                     help="Project name used to track experiments.Default set to 'cs6910_assignment3'.")
 parser.add_argument('-we','--wandb_entity',default="ep19b005",
                     help="Wandb entity used to track experiments in the W&B dashboard.Default set to 'ep19b005'.")
-parser.add_argument('-e','--max_epoch',default=15,type=int,help="Number of epochs to train the network.Supported type 'int'.Default set to 15")
-parser.add_argument('-b','--batch_size',default=64,type=int,
-                    help="Batch size used to train the network.Supported type 'int'.Default set to 64")
+parser.add_argument('-e','--max_epoch',default=30,type=int,help="Number of epochs to train the network.Supported type 'int'.Default set to 30")
+parser.add_argument('-b','--batch_size',default=32,type=int,
+                    help="Batch size used to train the network.Supported type 'int'.Default set to 32")
 parser.add_argument('-l','--loss',default="cross_entropy",choices=["cross_entropy"],
                     help="""Loss function used to train the neural network.Only supported choice is 'cross_entropy'.Default set to 'cross_entropy'""")
 parser.add_argument('-o','--optimizer',default="Adam",choices=["SGD","Nesterov","Adamax","RMSprop","NAdam","Adam"],help="""Optimizer used for training.Supported choices are 'SGD','Nesterov',
@@ -18,19 +21,19 @@ Beta1 used by SGD and Nesterov as momentum parameter.Beta1 used by RMSprop as al
 parser.add_argument('-beta2','--beta2',default=0.999,type=float,help="Beta2 used by Adam,Nadam and Adamax optimizer.Supported type 'float'.Default set to 0.999")
 parser.add_argument('-eps','--epsilon',default=10**-8,type=float,help="Epsilon used by optimizers.Supported type 'float'.Default set to 10**-8")
 parser.add_argument('-w_d','--weight_decay',default=0.001,type=float,help="L2 regularisation parameter or decay parameter used during training.Supported type 'float'.Default set to 0.001")
-parser.add_argument('-hl','--num_of_hidden_layers',default=5,type=int,help="""Number of hidden layers in the encoder-decoder network.Both encoder and decoder models take 
-the same number of hidden layers to prevent 'bottleneck' during passing of hidden states..Supported type 'int'.Default set to 5""")
-parser.add_argument('-sz','--hidden_size',default=64,type=int,help="Size of the hidden layer for both encoder & decoder networks.Supported type 'int'.Default set to 64.")
-parser.add_argument('-cell','--cell_type',default="rnn",choices=["rnn","gru","lstm"],help=""" The type of encoder decoder architecture 
-or hidden cell type.For relu activation.Default set to 'rnn'.""")
+parser.add_argument('-hl','--num_of_hidden_layers',default=3,type=int,help="""Number of hidden layers in the encoder-decoder network.Both encoder and decoder models take 
+the same number of hidden layers to prevent 'bottleneck' during passing of hidden states..Supported type 'int'.Default set to 3""")
+parser.add_argument('-sz','--hidden_size',default=256,type=int,help="Size of the hidden layer for both encoder & decoder networks.Supported type 'int'.Default set to 256.")
+parser.add_argument('-cell','--cell_type',default="lstm",choices=["rnn","gru","lstm"],help=""" The type of encoder decoder architecture 
+or hidden cell type.For relu activation.Default set to 'lstm'.""")
 parser.add_argument('-enc_em','--enc_embed_size',type=int,default=256,help="""The embedding size used by encoder network.Supported type 'int'.Default set to 256.""")
-parser.add_argument('-dec_em','--dec_embed_size',type=int,default=256,help="""The embedding size used by decoder network.Supported type 'int'.Default set to 256.""")
-parser.add_argument('-drop','--dropout',type=float,default=0.2,help="""The probability of switch-off in the dropout layers used in encoder and decoder networks.
+parser.add_argument('-dec_em','--dec_embed_size',type=int,default=128,help="""The embedding size used by decoder network.Supported type 'int'.Default set to 128.""")
+parser.add_argument('-drop','--dropout',type=float,default=0.3,help="""The probability of switch-off in the dropout layers used in encoder and decoder networks.
 Supported type 'float' from 0 to 1.Default set to 0.3""")
-parser.add_argument('-bi','--bidirectional',dest="bidirectional",action="store_false",help="""Boolean flag (action='store_false') with a default True value. Implements a bidirectional encoder network by default.
+parser.add_argument('-n0_bi','--no_bidirectional',dest="bidirectional",action="store_false",help="""Boolean flag (action='store_false') with a default True value. Implements a bidirectional encoder network by default.
 When flagged/called(becomes false),encoder network/model is no longer has bi-directional information/capability""")
-parser.add_argument('-es','--early_stopping',dest="early_stopping",action="store_true",help="""Boolean flag (action='store_true') with a default False value.Do early stopping during training by tracking validation 
-losses when set to True""")
+parser.add_argument('-no_es','--no_early_stopping',dest="early_stopping",action="store_false",help="""Boolean flag (action='store_false') with a default True value.No early stopping during training by tracking validation 
+losses when called/flagged(becomes False).By default, early stopping is set to True. """)
 parser.add_argument('-pat','--patience',default=4,type=int,help="""patience to be used while tracking validation loss for early stopping.Put to use only if 
 'early_stopping argument' is set to True.Supported type 'int'.Default set to 4. """)
 parser.add_argument('-no_log','--no_wandb_log',dest="wandb_log",action='store_false',help=""" Boolean flag (action='store_false') with a default True value.It allows logging of training loss,training accuracy,
@@ -39,25 +42,24 @@ validation accuracy and validation loss (to a already initialized wandb run) dur
 parser.add_argument('-tf','--teacher_forcing',default=0.6,type=float,help=""" Sets the percentage of the total no. of epochs for which teacher forcing
 will be enabled during training.Supported type 'float':neg values,no forcing & values greater than or equal to 1,forcing during all the epochs(entire training).
 Default set to 0.6""")
-parser.add_argument('-beam','--beam_search',default=0,type=int,help=""" When set to 0,does not do beam search during testing/inference on test data.When
-set to non-zero positive integer values, does beam search during testing/inference with the provided integer value as beam width.
-Supported type 'int':Non-negative integers.Default set to 0""")
-args=parser.parse_args('-att','--attn_mech',dest="attn_mech",action="store_false",help="""Boolean flag (action='store_false') with a default True value.
+parser.add_argument('-no_att','--no_attn_mech',dest="attn_mech",action="store_false",help="""Boolean flag (action='store_false') with a default True value.
 Implements a decoder network with attn_mechanism by default.When called/flagged(becomes false),decoder without attention mechanism is implemented.Default set to True""")
+
+args=parser.parse_args()
 
 from encoder_decoder import encoder,decoder,attention_decoder
 from data_loader_vocab_builder import *
 from main import training,compute_loss_accuracy
 
-print("loading Tamil dataset")
+#loading the Tamil dataset
+print("loading Tamil dataset...")
 (latin_script,latin_script_idx2word,dev_script_word2idx,dev_script_idx2word),(
     train_data,max_length_x,max_length_y),(val_data,max_length_val_x,max_length_val_y),(
     test_data,max_length_test_x,max_length_test_y)=load_data()
-print("dataset_loaded")
+print("dataset_loaded!!!")
 
 
-
-if args.wandb_log:
+if args.wandb_log: #if wandb_log argument True, the training run is logged to wandb, else only printing of results
     import wandb
     wandb.login()
     configuration={
@@ -98,12 +100,13 @@ if args.wandb_log:
                                                                                                  )
     run.name=name
 
-
+#instanstiate encoder model with commandline parameters 
 enc = encoder(hidden_size=args.hidden_size,num_of_hidden_layers=args.num_of_hidden_layers,dict_size=len(latin_script),
               embedding_size= args.enc_embed_size,cell_type= args.cell_type,
               bidirectional= args.bidirectional,dropout= args.dropout)
 enc.to(device=device)
-    
+
+#instanstiate dec model (attn /no attn) with commandline parameters 
 if not args.attn_mech:
     dec = decoder(args.hidden_size,
     num_of_hidden_layers= args.num_of_hidden_layers,dict_size= len(dev_script_word2idx),
@@ -113,26 +116,27 @@ if not args.attn_mech:
 else:
     dec = attention_decoder(hidden_size= args.hidden_size,
                             num_of_hidden_layers=args.num_of_hidden_layers,dict_size=len(dev_script_idx2word),
-                            embedding_size= args.dec_embedd_size,
+                            embedding_size= args.dec_embed_size,
                             cell_type=args.cell_type,
                             encoder_hidden_size=args.hidden_size,
                             dropout= args.dropout)
 dec.to(device=device)
 
-
+#train the model
+print("training started....")
 training(enc=enc,dec=dec,dev_script_word2idx=dev_script_word2idx,train_data=train_data,val_data=val_data,max_length_x=max_length_x,
          max_length_val_x=max_length_val_x,loss_func=args.loss,optimizer=args.optimizer,max_epoch=args.max_epoch,batch_size=args.batch_size,
-         learning_rate=args.learning_rate,beta_1=args.beta1,beta_2=args.beta_2,epsilon=args.epsilon,wei_decay=args.weight_decay,
+         learning_rate=args.learning_rate,beta_1=args.beta1,beta_2=args.beta2,epsilon=args.epsilon,wei_decay=args.weight_decay,
          early_stopping=args.early_stopping,patience=args.patience,wandb_log=args.wandb_log,teacher_forcing=args.teacher_forcing)
 
 if args.wandb_log:
     wandb.finish()
 else:
     pass
-
+#call compute loss accuracy on the trained model for testing inference
 acc,_=compute_loss_accuracy(x=test_data[:,:max_length_test_x],y=test_data[:,max_length_test_x:-2],enc1=enc,dec1=dec,seq_len_x=test_data[:,-2],
-                      seq_len_y=test_data[:,-1],dev_script_word2idx=dev_script_word2idx,testing=True,beam_search=args.beam_search)
+                      seq_len_y=test_data[:,-1],dev_script_word2idx=dev_script_word2idx,testing=True)
 
-print("Tamil Dataset")
-print("=>{} Test Accuracy:",acc)
+#printing test accuracy
+print("=>Test Accuracy: {}".format(acc))
 print("-"*200)
