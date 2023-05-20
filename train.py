a8ea4bbc5027c69da13008bf21a83e3728c3cc56
Muhi0709 <62468783+Muhi0709@users.noreply.github.com>
2023-05-20 20:20:59 +0530

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import argparse
 #define argsparse arguments necessary for 'training' and 'compute_loss_accuracy'functions from main.py
 parser=argparse.ArgumentParser(prog="Training & Testing NN model",description="""Training and Testing the encoder-decoder network on the 
 given aksharantar dataset.All parse arguments have default values and are set to the best configuration obtained from wandb sweep experiments""")
-parser.add_argument('-wp','--wandb_project',default="cs6910_assignment3",
+parser.add_argument('-wp','--wandb_project',default="assignment3_cs6910",
                     help="Project name used to track experiments.Default set to 'cs6910_assignment3'.")
 parser.add_argument('-we','--wandb_entity',default="ep19b005",
                     help="Wandb entity used to track experiments in the W&B dashboard.Default set to 'ep19b005'.")
